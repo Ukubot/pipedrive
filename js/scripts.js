1d5e649b90023db7756e11d547ce4e2a246a4f0a
Uku <Ukusaarm@gmail.com>
2016-02-17 01:25:34 +0200

    url changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 window.App = {};
 window.App.events = _.extend({}, Backbone.Events);
 
+var Router = Backbone.Router.extend({
+  routes: {
+    "profile/:id": "getProfile"
+  }
+});
+
+window.App.router = new Router();
+
+window.App.router.on("route:getProfile", function(id) {
+  console.log("get profile: " + id);
+});
+
+Backbone.history.start();
+
 var User = Backbone.Model.extend();
 
 var Users = Backbone.Collection.extend({
@@ -35,10 +49,43 @@ var UsersView = Backbone.View.extend({
     var userModel = _.find(models, function(m) {
       return m.id == id;
     });
-    App.events.trigger("user-selected", userModel.attributes);
+    var user = userModel.attributes;
+    App.events.trigger("user-selected", user);
+    App.router.navigate("/profile/" + user.id);
   },
 
   template: _.template($("#usersTemplate").html()),
+
+  render: function() {
+    $(this.el).html(this.template({ users: this.collection.toJSON() }));
+  }
+});
+
+
+var Deal = Backbone.Model.extend();
+
+var Deals = Backbone.Collection.extend({
+  model: Deal,
+  url: "https://api.pipedrive.com/v1/deals?start=0&api_token=678ff83eb61acc57410ea663bbcbf25b651d787c",
+  parse: function(response) {
+    console.log(response);
+    return response.data;
+  }
+});
+
+var DealsView = Backbone.View.extend({
+  model: Deal,
+  initialize: function() {
+    this.collection = new Deals;
+
+    var that = this;
+    this.collection.fetch({
+      success: function() {
+        that.render();
+      }
+    });
+  },
+  template: _.template($("#dealsTemplate").html()),
 
   render: function() {
     $(this.el).html(this.template({ users: this.collection.toJSON() }));
@@ -55,6 +102,8 @@ var ProfileView = Backbone.View.extend({
     }, this);
   },
 
+  template: _.template($("#profileTemplate").html()),
+
   render: function(user) {
     $(this.el).html(this.template({ user: user }));
   }
@@ -62,6 +111,10 @@ var ProfileView = Backbone.View.extend({
 
 var users = new UsersView({
   el: $("#users-list")
+});
+
+var deals = new DealsView({
+  el: $("#deals-list")
 });
 
 var profile = new ProfileView({
